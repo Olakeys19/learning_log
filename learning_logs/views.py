77c12e5bd2f1e778f36ea8af0dd389de424a285a
olakeys <olas15258@gmail.com>
2026-05-14 14:31:25 +0200

    Refined custom 404 and 500 error pages.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -26,7 +26,7 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries"""
     topic = get_object_or_404(Topic, id=topic_id)
-    
+
     check_topic_owner(topic, request)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
@@ -53,7 +53,8 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
+
     check_topic_owner(topic, request)
     if request.method != 'POST':
         # No data submitted; create a blank form.
@@ -73,7 +74,7 @@ def new_entry(request, topic_id):
 @login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
-    entry = Entry.objects.get(id=entry_id)
+    entry = get_object_or_404(Entry, id=entry_id)
     topic = entry.topic
     check_topic_owner(topic, request)
     if request.method != 'POST':
